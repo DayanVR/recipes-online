f9fdeebdc8db0984c1e3b8d96551db9893e8528a
Dayan Villavicencio Rolffot <108312281+DayanVR@users.noreply.github.com>
2024-08-12 19:42:19 -0400

    fixed typescript error

--- src/components/separatedByMeal.ts
+++ src/components/separatedByMeal.ts
@@ -1,13 +1,21 @@
 import type { ProductInfo } from "../types.js";
 
-export default function separateByMeal(meals: [ProductInfo]) {
-  return meals.reduce((result, meal) => {
-    meal.mealType.forEach((type) => {
+type MealCategory = {
+  [key: string]: ProductInfo[];
+};
+
+export default function separateByMeal(meals: ProductInfo[]): MealCategory {
+  return meals.reduce<MealCategory>((result, meal) => {
+    // Ensure meal.mealType is an array
+    const mealTypes = Array.isArray(meal.mealType) ? meal.mealType : [meal.mealType];
+    
+    mealTypes.forEach((type) => {
       if (!result[type]) {
         result[type] = [];
       }
       result[type].push(meal);
     });
+
     return result;
-  }, {});
+  }, {} as MealCategory);
 }
